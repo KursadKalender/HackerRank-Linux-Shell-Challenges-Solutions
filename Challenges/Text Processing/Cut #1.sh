7f504cb3a1ba6a4bc0db2a80a390f4d999bfd8f4
Muhammed Kürşad Kalender <72703317+KursadKalender@users.noreply.github.com>
2023-04-12 07:34:53 +0300

    Update Cut #1.sh

--- Challenges/Text Processing/Cut #1.sh
+++ Challenges/Text Processing/Cut #1.sh
@@ -1,3 +1,5 @@
+#Given  lines of input, print the 3rd character from each line as a new line of output. It is guaranteed that each of the  lines of input will have a 3rd character.
+
 #echo THE WORD | head -c 3 | tail -c 1
 
 cut -c 3
