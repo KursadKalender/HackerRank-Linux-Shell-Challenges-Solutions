c75abb2c3482d75fb92b05238ad7ce537b4766b0
Muhammed Kürşad Kalender <72703317+KursadKalender@users.noreply.github.com>
2023-04-15 06:48:19 +0300

    Create 'Tr' Command #2.sh

---
+++ Challenges/Text Processing/'Tr' Command #2.sh
@@ -0,0 +1,7 @@
+#In this challenge, we practice using the tr command because it is a useful translation tool in Linux.
+
+#In a given fragment of text, delete all the lowercase characters a-z.
+
+#-d parameter is used to delete characters.
+
+tr -d 'a-z'
