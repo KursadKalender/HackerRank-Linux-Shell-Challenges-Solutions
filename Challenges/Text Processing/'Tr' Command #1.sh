4f38863ef77018c0c17a4229b27bd383367205db
Muhammed Kürşad Kalender <72703317+KursadKalender@users.noreply.github.com>
2023-04-15 06:44:58 +0300

    Create 'Tr' Command #1.sh

---
+++ Challenges/Text Processing/'Tr' Command #1.sh
@@ -0,0 +1,5 @@
+#In this challenge, we practice using the tr command because it is a useful translation tool in Linux.
+
+#In a given fragment of text, replace all parentheses () with box brackets [] .
+
+tr '()' '[]'
