d44168217591980ce1fe21b31d3b82a2c0f7903e
Muhammed Kürşad Kalender <72703317+KursadKalender@users.noreply.github.com>
2023-04-16 08:31:53 +0300

    Create Sort Command #4.sh

---
+++ Challenges/Text Processing/Sort Command #4.sh
@@ -0,0 +1,5 @@
+#You are given a file of text, where each line contains a number (which may be either an integer or have decimal places). There will be no extra characters other than the number or the newline at the end of each line. Sort the lines in descending order - - such that the first line holds the (numerically) largest number and the last line holds the (numerically) smallest number.
+
+#-n and -r parameters are both used in combination to sort in reverse order.
+
+sort -n -r
