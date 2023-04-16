7b58c4d02c99fb723eb75cc8965968b91f29487a
Muhammed Kürşad Kalender <72703317+KursadKalender@users.noreply.github.com>
2023-04-16 08:38:49 +0300

    Create Sort Command #5.sh

---
+++ Challenges/Text Processing/Sort Command #5.sh
@@ -0,0 +1,3 @@
+#You are given a file of text,which contains temperature information about American cities, in TSV (tab-separated) format. The first column is the name of the city and the next four columns are the average temperature in the months of Jan, Feb, March and April (see the sample input). Rearrange the rows of the table in descending order of the values for the average temperature in January.
+
+sort -k2 -n -r -t $'\t'
