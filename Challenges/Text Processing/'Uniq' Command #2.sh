638722a75c88be213f7feb7aefb52c4cc9407941
Muhammed Kürşad Kalender <72703317+KursadKalender@users.noreply.github.com>
2023-04-17 03:38:37 +0300

    Create 'Uniq' Command #2.sh

---
+++ Challenges/Text Processing/'Uniq' Command #2.sh
@@ -0,0 +1,5 @@
+#In this challenge, we practice using the uniq command to eliminate consecutive repetitions of a line when a text file is piped through it.
+
+#Given a text file, count the number of times each line repeats itself. Only consider consecutive repetitions. Display the space separated count and line, respectively. There shouldn't be any leading or trailing spaces. Please note that the uniq -c command by itself will generate the output in a different format than the one expected here.
+
+uniq -c | cut -c7-
