3f6891516bf891dbfb020de5e0c4c8ce06b927a7
Muhammed Kürşad Kalender <72703317+KursadKalender@users.noreply.github.com>
2023-04-19 08:00:41 +0300

    Create 'Grep' #1.sh

---
+++ Challenges/Grep Sed Awk/'Grep' #1.sh
@@ -0,0 +1,5 @@
+#You are given a text file that will be piped into your command through STDIN. Use grep to display all the lines that contain the word the in them. The search should be sensitive to case. Display only those lines of the input file that contain the word 'the'.
+
+#-w parameter is used to search for the exact word 'the'.
+
+grep -w 'the'
