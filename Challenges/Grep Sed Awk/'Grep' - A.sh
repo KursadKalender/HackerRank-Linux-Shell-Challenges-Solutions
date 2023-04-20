6ef0ce2d5395dde2e29de68331bcda68eda62510
Muhammed Kürşad Kalender <72703317+KursadKalender@users.noreply.github.com>
2023-04-20 07:49:57 +0300

    Create 'Grep' - A.sh

---
+++ Challenges/Grep Sed Awk/'Grep' - A.sh
@@ -0,0 +1,10 @@
+#Given a text file, which will be piped to your command through STDIN, use grep to display all those lines which contain any of the following words in them:
+#the
+#that
+#then
+#those
+#The search should not be sensitive to case. Display only those lines of an input file, which contain the required words.
+
+#'\|'' this pattern is used between wanted words.
+
+grep -iw 'the\|that\|then\|those'
