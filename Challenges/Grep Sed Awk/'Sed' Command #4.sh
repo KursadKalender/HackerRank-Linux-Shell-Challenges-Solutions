76013a3f66d6707fb2b361c5475407a00aa2e109
Muhammed Kürşad Kalender <72703317+KursadKalender@users.noreply.github.com>
2023-04-21 05:27:18 +0300

    Create 'Sed' Command #4.sh

---
+++ Challenges/Grep Sed Awk/'Sed' Command #4.sh
@@ -0,0 +1,3 @@
+#Given n lines of credit card numbers, mask the first 12 digits of each credit card number with an asterisk (i.e., *) and print the masked card number on a new line. Each credit card number consists of four space-separated groups of four digits. For example, the credit card number 1234 5678 9101 1234 would be masked and printed as **** **** **** 1234.
+
+sed -r 's/[0-9]{4}[ ]/**** /g'
